30ed0be78611ee235e9d621d22c726d911ea426b
Zach Petch <zachpetch@gmail.com>
2019-07-09 20:03:57 -0700

    Initial commit

---
+++ src/poet/haiku/Poet.java
@@ -0,0 +1,10 @@
+package poet.haiku;
+
+public class Poet {
+    public static void main(String[] args)
+    {
+        System.out.println("This dew drop world");
+        System.out.println("Is a dew drop world and yet");
+        System.out.println("And yet");
+    }
+}
